553f09c4fc30355c95443f127ea3a418a71ea54c
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-11-13 21:04:10 +0530

    Create leftmost-and-rightmost-node-of-bst.java

---
+++ tree/leftmost-and-rightmost-node-of-bst.java
@@ -0,0 +1,27 @@
+class Tree
+{
+    public static void printCorner(Node node)
+    {
+        
+        // add your code here  
+        if(node==null) return;
+        Queue<Node> q=new LinkedList<>();
+        q.add(node);
+        
+        while(q.isEmpty()==false){
+            int n=q.size();
+            
+            for(int i=1; i<=n; i++){
+                Node curr=q.poll();
+                if(i==1||i==n){    //adding first and last element of the queue
+                    System.out.print(curr.data+" ");
+                }
+                if(curr.left!=null)
+                q.add(curr.left);
+                if(curr.right!=null)
+                q.add(curr.right);
+            }
+        }
+    }
+    
+}

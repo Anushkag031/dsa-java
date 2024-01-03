36d254176211122552846cea366d82e7a9c2d3c5
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2024-01-03 14:26:07 +0530

    Create all-no-disappeared-in-array.java

---
+++ array/all-no-disappeared-in-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        ArrayList<Integer> al=new ArrayList<>();
+         
+         for(int i : nums)
+         set.add(i);
+
+         for(int i=1; i<=nums.length; i++)
+         {
+             if(!set.contains(i))
+             al.add(i);
+         }
+         return al;
+    }
+}
